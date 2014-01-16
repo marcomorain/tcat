862041ea8886b04a6d594ba52fee7b8df520d590
Marc O'Morain <marc.omorain@swrve.com>
2014-01-16 17:19:19 +0000

    better error checking

--- tcat.c
+++ tcat.c
@@ -3,11 +3,11 @@
 #include <time.h>
 #include <assert.h>
 
-static void output_error() {
-  if (feof(stdout)) {
-    fputs("stdout closed", stderr);
-  } else if (ferror(stdout)) {
-    perror("error writing to stdout");
+static void io_error(FILE* file) {
+  if (feof(file)) {
+    fputs("file closed", stderr);
+  } else if (ferror(file)) {
+    perror("file error");
   } else {
     fputs("unknown error", stderr);
   }
@@ -15,14 +15,14 @@ static void output_error() {
 }
 
 static void print_time() {
-  enum { buffer_max = 20 };
+  enum { buffer_max = 32 };
   char buffer[buffer_max];
   time_t rawtime;
   time(&rawtime);
   struct tm* timeinfo = localtime(&rawtime);
-  const size_t len = strftime(buffer, buffer_max, "%Y:%m:%d%H:%M:%S\t", timeinfo);
+  const size_t len = strftime(buffer, buffer_max, "%Y:%m:%d %H:%M:%S\t", timeinfo);
   if(len != fwrite(buffer, 1, len, stdout)){
-    output_error();
+    io_error(stdout);
   }
 }
 
@@ -30,17 +30,17 @@ int main(int argc, char** argv) {
   int last = '\n';
   int c;
   for(c = fgetc(stdin); c != EOF; c = fgetc(stdin)) {
-
     if (last == '\n') {
       print_time();
     }
-
     if (EOF == fputc(c, stdout)) {
-      output_error();
+      io_error(stdout);
     }
-
     last = c;
   }
 
+  if (feof(stdin)) {
+    io_error(stdin);
+  }
   return EXIT_SUCCESS;
 }
